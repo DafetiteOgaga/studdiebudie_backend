bc86629695272b68ece5e689396f749f86c01c41
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-14 08:58:04 +0100

    views.py: added to the httpresponse data

--- defaultpage/views.py
+++ defaultpage/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+welcome = """
+	Welcome to the Study Buddie Backend!<br/>
+	Remember that the paths in the config starts with the appname<br/>
+	And it continues with specific path names in individual apps.
+"""
 # Create your views here.
 def defaultpage(request):
 	"""
 	Default page view that renders the default page template.
 	"""
-	return HttpResponse("Welcome to the Study Buddie Backend! This is the default page. Please navigate to the appropriate section using the provided links.")
+	return HttpResponse(welcome)
